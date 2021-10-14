d8be301d4adfc57a5e76d68f4182fc279b0f742c
Peter Chermanowicz <ch1_git_762e4c1c@eml.cc>
2021-10-14 21:50:45 +0100

    Request class added (WIP)

---
+++ src/api/endpoints.ts
@@ -0,0 +1,6 @@
+export enum endpoints {
+  IMAGES = "images",
+  UPLOAD = "images/upload",
+  VOTE = "votes",
+  FAVOURITES = "favourites"
+}
---
+++ src/api/request.ts
@@ -0,0 +1,71 @@
+import axios from "axios";
+import axiosRetry, { exponentialDelay } from "axios-retry";
+import { HttpMethod } from "../models/api";
+
+export class Request {
+  constructor() {
+    axiosRetry(axios, { retries: 3, retryDelay: exponentialDelay });
+  }
+
+  public async get(url: string): Promise<any> {
+    return await axios
+      .get(url, this.getRequestParams(HttpMethod.GET))
+      .then(this.handleResponse)
+      .catch(this.handleError);
+  }
+
+  public async post(url: string, data = {}): Promise<any> {
+    return await axios
+      .post(url, data, this.getRequestParams(HttpMethod.POST))
+      .then(this.handleResponse)
+      .catch(this.handleError);
+  }
+
+  public async put(url: string, data = {}): Promise<any> {
+    return await axios
+      .put(url, data, this.getRequestParams(HttpMethod.PUT))
+      .then(this.handleResponse)
+      .catch(this.handleError);
+  }
+
+  public async delete(url: string): Promise<any> {
+    return await axios
+      .delete(url, this.getRequestParams(HttpMethod.DELETE))
+      .then(this.handleResponse)
+      .catch(this.handleError);
+  }
+
+  private async handleResponse(response: any): Promise<any> {
+    const { status } = response;
+    const isSuccess: boolean = status >= 200 && status < 300;
+    if (isSuccess) {
+      return response.data;
+    }
+
+    return Promise.reject({
+      errorCode: response.status,
+      errorText: response.statusText
+    });
+  }
+
+  private async handleError(error: Response) {
+    return Promise.reject(error);
+  }
+
+  private getRequestParams(method: HttpMethod) {
+    return {
+      method,
+      mode: "cors" as RequestMode,
+      cache: "no-cache" as RequestCache,
+      credentials: "same-origin" as RequestCredentials,
+      redirect: "follow" as RequestRedirect,
+      referrer: "no-referrer",
+      withCredentials: true,
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+        "x-api-key": process.env.REACT_APP_API_TOKEN
+      } as any
+    };
+  }
+}
